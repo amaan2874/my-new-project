301957096a401d997e3b7b95e4e837405c6002a6
farhan2874 <amaanraza4905@gmail.com>
2024-03-12 23:15:43 +0530

    add dashboard

---
+++ script.js
@@ -0,0 +1,18 @@
+function submit(){
+    var email = document.getElementById("email").value
+    var pass = document.getElementById("password").value
+    // var check = document.getElementById("remember-me").value
+    var islogin = false
+
+    if(email == "arfa333@gmail.com" && pass == 1234){
+        islogin = true
+        if(islogin){
+            location.replace
+        }
+    }
+    console.log("islogin :"+islogin)
+    console.log("Email : "+email)
+    console.log("password : " +pass)
+    // console.log(check)
+
+}
